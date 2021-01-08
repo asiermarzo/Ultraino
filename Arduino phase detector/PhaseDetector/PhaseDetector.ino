4a949291c763b54879ef074ddb65bcbd2a74a94e
Asier Marzo <amarzo@hotmail.com>
2021-01-08 12:12:54 +0100

    Update PhaseDetector.ino

--- Arduino phase detector/PhaseDetector/PhaseDetector.ino
+++ Arduino phase detector/PhaseDetector/PhaseDetector.ino
@@ -1,11 +1,11 @@
 void setup() {
   //config PINS
-  pinMode(2, INPUT_PULLUP); //Sync (from the board) -> D2 (PD2)
+  pinMode(2, INPUT_PULLUP); //Sync (from the driver board) -> D2 (PD2)
 
   //set the ADC reference to 5v
   //analogReference(EXTERNAL);
   
-  //set the ADC as fast as possible, setting the prescale to 16
+  //set the ADC as fast as possible, setting the prescale to 4 (around 307ks)
   ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
   ADCSRA |= bit (ADPS1);                               //   4 
   
@@ -14,11 +14,11 @@ void setup() {
 }
 
 #define N_SAMPLES 32
+int samples[N_SAMPLES];
 void loop() {
-   int samples[N_SAMPLES];
- 
-  //wait for one byte from the serial to start capture
+  //wait for one byte from the serial to start the capture
   if(Serial.available() > 0){
+    //wait for a falling edge of the sync input
     while (PIND & 0b00000100); //wait for the sync to go up
     while (! (PIND & 0b00000100) ); //wait for the sync to go down
 
@@ -27,13 +27,13 @@ void loop() {
       samples[i] = analogRead(A0);
     }
     
-    //send them
+    //send them through Serial, each sample as uint16
     for(int i = 0; i<N_SAMPLES; ++i){
       Serial.write( (samples[i] >> 8) & 0xFF );
       Serial.write( samples[i] & 0xFF );
       //Serial.println(samples[i]);
     }
     
-    Serial.read(); //discard the serial byte
+    Serial.read(); //discard the byte that was sent to us 
   }
 }
